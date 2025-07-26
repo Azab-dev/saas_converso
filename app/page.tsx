f2b9b6d662e429f6b9ec827f7953146403d152fc
mostafa <mostafaazab73@gmail.com>
2025-07-26 19:42:59 +0300

    Fix dynamic page issue on home

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ import {
   getRecentSessions,
 } from "@/lib/actions/companion.actions";
 
+
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
   const companions = await getAllCompanions({ limit: 3 });
   const recentSessionsCompanions = await getRecentSessions(10);
